026c5ff612dc410e9b65eabe29113ecd18e09944
Duc Nguyen <dnguyen416@gatech.edu>
2023-11-25 23:59:58 -0500

    refractor: port .env

--- server/server.js
+++ server/server.js
@@ -35,4 +35,6 @@ app.get("/", (req, res) => {
   res.redirect(process.env.CLIENT_URL);
 });
 
-app.listen(3000, () => console.log("server is running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("server is running on port 3000"));
