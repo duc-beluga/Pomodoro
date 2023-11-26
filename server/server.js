8b5f2d2c7bfd35c55e40cedbe4fb054c7489f28a
Duc Nguyen <dnguyen416@gatech.edu>
2023-11-25 22:31:08 -0500

    debug: serialize/deserialize config

--- server/server.js
+++ server/server.js
@@ -21,10 +21,12 @@ app.use(passport.session());
 passport.use(GitHub);
 
 passport.serializeUser((user, done) => {
+  console.log("serialize", user);
   done(null, user);
 });
 
 passport.deserializeUser((user, done) => {
+  console.log("deserialize", user);
   done(null, user);
 });
 
